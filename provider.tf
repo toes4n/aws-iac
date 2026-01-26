7e912f6bf19b75d26914bb966736994342a2bafa
TS <ts@ts>
2026-01-26 18:09:29 +0700

    update provider

--- provider.tf
+++ provider.tf
@@ -7,19 +7,24 @@ provider "aws" {
   }
 }
 
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
 provider "helm" {
   kubernetes {
     host                   = module.eks.cluster_endpoint
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-
-    exec {
-      api_version = "client.authentication.k8s.io/v1beta1"
-      args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name, "--region", var.aws_region]
-      command     = "aws"
-    }
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
 
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+}
 
 terraform {
   backend "s3" {
